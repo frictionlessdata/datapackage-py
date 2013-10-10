8ab2eb7086d8bda2f6d7082071b70a34156f2e2f
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-10-10 21:04:37 +0000

    Version 0.2. Minor version update since we now support a little bit more of the Data Package standard (encoding of resources). Thanks to Michael Bauer.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme:
 
 setup(
     name = 'datapackage',
-    version = '0.1.3',
+    version = '0.2.0',
     url = 'https://github.com/tryggvib/datapackage',
     license = 'GPLv3',
     description = description,
