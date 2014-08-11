59b9344b88fd873d272c9c8adfb31f6963c17ea6
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-08-11 15:43:28 -0700

    Add docstring to verify_semantic_version

--- datapackage/util.py
+++ datapackage/util.py
@@ -5,9 +5,14 @@ if sys.version_info[0] < 3:
     str = unicode
 
 
-def verify_semantic_version(val):
-    parts = val.split('.')
+def verify_semantic_version(version):
+    """Verify that semantic versioning (http://semver.org/) is being
+    followed. In particular, the version should have a major version,
+    minor version, and patch version, separated by periods.
+
+    """
+    parts = version.split('.')
     if len(parts) != 3:
         raise ValueError(
-            "version '{}' does not follow semantic versioning".format(val))
-    return str(val)
+            "version '{}' does not follow semantic versioning".format(version))
+    return str(version)
