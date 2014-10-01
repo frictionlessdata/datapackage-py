f9f7e4aecb8d378e42237a643029cf002a303bca
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-10-01 11:11:35 +0000

    Version 0.4.1
    Bug fix in data retrieval based on different structure of
    the resources property.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme:
 
 setup(
     name = 'datapackage',
-    version = '0.4.0',
+    version = '0.4.1',
     url = 'https://github.com/tryggvib/datapackage',
     license = 'GPLv3',
     description = description,
