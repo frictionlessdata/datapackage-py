c8cfdc4e13e0cea3a1b847bd748c67c9154bf127
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-10-01 09:12:48 +0000

    Version 0.4.0
    This version adds write capability to data packages so users can
    now use the module to both read and write data packages. With this
    version we also get proper documentation, tests, data package
    objects like Schema, the beginnings of a data package validation
    and much more.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme:
 
 setup(
     name = 'datapackage',
-    version = '0.3.1',
+    version = '0.4.0',
     url = 'https://github.com/tryggvib/datapackage',
     license = 'GPLv3',
     description = description,
