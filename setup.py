17141e27638213f4e6adfedf8e3fa8aaf69a7f4b
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2015-04-09 01:07:05 +0000

    Patch version 0.5.1
    * Exclude non datapackage fields (internal fields)
        * datapackage_uri
        * is_local

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with io.open('README.rst') as readme:
 
 setup(
     name = 'datapackage',
-    version = '0.5.0',
+    version = '0.5.1',
     url = 'https://github.com/tryggvib/datapackage',
     license = 'GPLv3',
     description = description,
