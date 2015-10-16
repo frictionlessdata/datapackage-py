40cc0f89fdfece3492cd7c952d22bd23f29ddf38
Brook Elgie <specialbrew@gmail.com>
2015-10-16 10:16:38 +0100

    Rename module to enable import by coverage.
    If the module `validate` has the same name as a child function it's
    trying to import, it's not clear whether we're importing the module of
    the function. This was upsetting running nosetests with coverage.
    Renaming `validate` module to `validation`.

--- datapackage_validate/__init__.py
+++ datapackage_validate/__init__.py
@@ -4,7 +4,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .validate import validate
+from .validation import validate
 
 
 __all__ = ['validate']
