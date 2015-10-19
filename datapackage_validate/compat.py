3c1cc9cc6db6d8fa067f4dc118dfc70bfd1bb4b6
Brook Elgie <specialbrew@gmail.com>
2015-10-19 13:41:06 +0100

    Remove unused compat code

--- datapackage_validate/compat.py
+++ datapackage_validate/compat.py
@@ -24,23 +24,6 @@ if is_py2:
     basestring = basestring
     numeric_types = (int, long, float)
 
-    def _iterenc_utf8(data):
-        for line in data:
-            yield line.encode('utf-8')
-
-    def csv_reader(data, dialect=csv.excel, **kwargs):
-        '''Read text stream (unicode on Py2.7) as CSV.'''
-        reader = csv.reader(_iterenc_utf8(data), dialect=dialect, **kwargs)
-        for row in reader:
-            yield [str(cell, 'utf-8') for cell in row]
-
-    def csv_dict_reader(data, dialect=csv.excel, **kwargs):
-        '''Read text stream as CSV, yielding dict objects'''
-        dict_reader = csv.DictReader(_iterenc_utf8(data), **kwargs)
-        for row in dict_reader:
-            yield {key: unicode(value, 'utf-8')
-                   for key, value in row.iteritems()}
-
 elif is_py3:
     from urllib import parse
     csv_reader = csv.reader
