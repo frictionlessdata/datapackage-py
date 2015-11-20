2d0f9661bb092397a739fa927abdc04cf04db22e
Vitor Baptista <vitor@vitorbaptista.com>
2015-11-20 10:00:15 +0000

    Import from __future__ to make it easier to write py2-compatible code

--- datapackage/datapackage.py
+++ datapackage/datapackage.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
-import six
 from .schema import Schema
 
 
@@ -27,7 +31,7 @@ class DataPackage(object):
         private_prefix = '_{cls}__'.format(cls=self.__class__.__name__)
         is_private = lambda k: k.startswith(private_prefix)
 
-        return {k: v for (k, v) in six.iteritems(self.__dict__)
+        return {k: v for (k, v) in self.__dict__.items()
                 if not is_private(k) and v is not None}
 
     def validate(self):
