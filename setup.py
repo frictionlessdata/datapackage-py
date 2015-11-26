00cff04de12557afdb812ba7e9c2daa23d01b274
Vitor Baptista <vitor@vitorbaptista.com>
2015-11-26 20:16:24 +0000

    [#5] Fix inclusion of schema into package
    I wasn't able to make it work just with the MANIFEST.in, and also
    `package_data` doesn't allow recursive patterns. I didn't want to consider that
    all schemas will be on the root folder, so I had to create a custom function.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from __future__ import division
 from __future__ import print_function
 
 import io
+import os
+import fnmatch
 
 from setuptools import setup, find_packages
 
@@ -18,6 +20,29 @@ dependencies = [
     'requests>=2.8.0',
 ]
 
+
+def schema_files():
+    '''Return all CSV and JSON files paths in datapackage_registry/schemas
+
+    The paths are relative to ./datapackage_registry
+    '''
+    def recursive_glob(path, patterns):
+        results = []
+        for base, dirs, files in os.walk(path):
+            matching_files = []
+            for pattern in patterns:
+                matching_files.extend(fnmatch.filter(files, pattern))
+            results.extend(os.path.join(base, f) for f in matching_files)
+        return results
+
+    base_folder = 'datapackage_registry'
+    remove_base_folder = lambda path: path[len(base_folder) + 1:]
+
+    path = os.path.join(base_folder, 'schemas')
+    files_paths = recursive_glob(path, ['*.csv', '*.json'])
+
+    return [remove_base_folder(f) for f in files_paths]
+
 setup(
     name='datapackage-registry',
     version='0.1.1',
@@ -29,6 +54,7 @@ setup(
     author_email='info@okfn.org',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     package_dir={'datapackage_registry': 'datapackage_registry'},
+    package_data={'datapackage_registry': schema_files()},
     install_requires=dependencies,
     classifiers=[
         'Development Status :: 4 - Beta',
