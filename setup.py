8ad3b5c9ed052d286c0e3f5c7d66c68a275f85b3
Vitor Baptista <vitor@vitorbaptista.com>
2015-11-27 13:51:11 +0000

    Bump version to 0.1.0
    This version depends on the upgraded datapackage_registry 0.1.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ dependencies = [
 
 setup(
     name='datapackage-validate',
-    version='0.0.3',
+    version='0.1.0',
     url='https://github.com/okfn/datapackage-validate-py',
     license='MIT',
     description=description,
