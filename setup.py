f5cc6b92bb9138ddda293d8d3884ede2288a08cc
Vitor Baptista <vitor@vitorbaptista.com>
2016-05-24 17:52:34 +0100

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def schema_files():
 
 setup(
     name='datapackage',
-    version='0.6.0',
+    version='0.6.1',
     description=(
         'Utilities to work with Data Packages as defined on dataprotocols.org'
     ),
