82e9863b24c8735fcb4b8780591477e187ab845f
roll <eskarev@gmail.com>
2017-05-03 15:06:39 +0200

    Fixed packaging

--- setup.py
+++ setup.py
@@ -9,29 +9,6 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-def schema_files():
-    '''Return all CSV and JSON files paths in datapackage/schemas
-
-    The paths are relative to ./datapackage
-    '''
-    def recursive_glob(path, patterns):
-        results = []
-        for base, dirs, files in os.walk(path):
-            matching_files = []
-            for pattern in patterns:
-                matching_files.extend(fnmatch.filter(files, pattern))
-            results.extend(os.path.join(base, f) for f in matching_files)
-        return results
-
-    base_folder = 'datapackage'
-    remove_base_folder = lambda path: path[len(base_folder) + 1:]
-
-    path = os.path.join(base_folder, 'schemas')
-    files_paths = recursive_glob(path, ['*.csv', '*.json'])
-
-    return [remove_base_folder(f) for f in files_paths]
-
-
 setup(
     name='datapackage',
     version='1.0.0-alpha2',
@@ -65,7 +42,7 @@ setup(
     keywords='data jsontableschema frictionlessdata datascience',
 
     packages=find_packages(exclude=['tests']),
-    package_data={'datapackage': schema_files()},
+    include_package_data=True,
 
     install_requires=[
         'six>=1.10,<2.0a',
