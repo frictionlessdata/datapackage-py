d8160e263c13027f98419084210c6f8214395285
roll <eskarev@gmail.com>
2017-05-03 16:03:41 +0200

    Fixed urljoin

--- datapackage/resource.py
+++ datapackage/resource.py
@@ -166,7 +166,7 @@ def _get_source_with_type(data, path, base_path):
             source = path[0]
             source_type = 'remote'
         elif base_path and base_path.startswith('http'):
-            source = urljoin(base_path, path[0])
+            source = urljoin(base_path + '/', path[0])
             source_type = 'remote'
         else:
             if not helpers.is_safe_path(path[0]):
