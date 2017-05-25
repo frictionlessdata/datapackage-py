237f35b23d2d9ae918b548e3c6afbb58712a3c2c
roll <eskarev@gmail.com>
2017-05-25 12:20:53 +0300

    v0.8.9

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def schema_files():
 
 setup(
     name='datapackage',
-    version='0.8.8',
+    version='0.8.9',
     description=(
         'Utilities to work with Data Packages as defined on dataprotocols.org'
     ),
