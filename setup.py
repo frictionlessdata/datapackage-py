925b70e12f3eca6f4e9ecf65a78696d57b39431a
roll <eskarev@gmail.com>
2017-05-25 11:41:52 +0300

    Updated tabulator dependency

--- setup.py
+++ setup.py
@@ -16,42 +16,33 @@ setup(
         'Utilities to work with Data Packages as defined on specs.frictionlessdata.io'
     ),
     long_description=long_description,
-
     url='https://github.com/frictionlessdata/datapackage-py',
-
     author='Open Knowledge Foundation',
     author_email='info@okfn.org',
-
     license='MIT',
-
     classifiers=[
         'Development Status :: 4 - Beta',
-
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Topic :: Utilities',
-
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-
     keywords='data tableschema frictionlessdata datascience',
-
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-
     install_requires=[
-        'six>=1.10,<2.0a',
-        'requests>=2.8,<3.0a',
-        'jsonschema>=2.5,<3.0a',
-        'unicodecsv>=0.14,<1.0a',
-        'jsontableschema>=0.8,<1.0a',
-        'tableschema>=1.0.0a4,<2.0',
-        'tabulator>=0.14.2,<1.0a',
-        'jsonpointer>=1.10,<2.0a',
+        'six>=1.10,<2.0',
+        'requests>=2.8,<3.0',
+        'jsonschema>=2.5,<3.0',
+        'unicodecsv>=0.14,<2.0',
+        'jsonpointer>=1.10,<2.0',
+        'tableschema>=1.0.0a5,<2.0',
+        'tabulator>=1.0.0a5,<2.0',
+        # TODO: remove after specs-v1
+        'jsontableschema>=0.8,<1.0',
     ],
 )
