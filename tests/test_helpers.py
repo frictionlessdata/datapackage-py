9c57269ce9a313f764ba3ce7f031b80f54c822ae
Vitor Baptista <vitor@vitorbaptista.com>
2017-09-27 20:34:52 +0100

    Paths pointing to "~" home dirs and with env variables are unsafe

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,7 +7,8 @@ from __future__ import unicode_literals
 import os
 import shutil
 import tempfile
-from datapackage.helpers import ensure_dir
+import pytest
+import datapackage.helpers as helpers
 
 
 # Tests
@@ -18,7 +19,26 @@ def test_ensure_dir():
         dir_path = os.path.join(base_path, 'dir')
         file_path = os.path.join(dir_path, 'file')
         assert not os.path.isdir(dir_path)
-        ensure_dir(file_path)
+        helpers.ensure_dir(file_path)
         assert os.path.isdir(dir_path)
     finally:
         shutil.rmtree(base_path)
+
+
+@pytest.mark.parametrize('path,is_safe', (
+    ('data.csv', True),
+    ('data/data.csv', True),
+    ('data/country/data.csv', True),
+    ('data\\data.csv', True),
+    ('data\\country\\data.csv', True),
+
+    ('../data.csv', False),
+    ('~/data.csv', False),
+    ('~invalid_user/data.csv', False),
+    ('%userprofile%', False),
+    ('%unknown_windows_var%', False),
+    ('$HOME', False),
+    ('$UNKNOWN_VAR', False),
+))
+def test_is_safe_path(path, is_safe):
+    assert helpers.is_safe_path(path) is is_safe
