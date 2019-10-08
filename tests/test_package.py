d27e06adfef02df179418451d7efb7e8980a279b
roll <eskarev@gmail.com>
2019-10-08 14:16:22 +0300

    Added save to json test

--- tests/test_package.py
+++ tests/test_package.py
@@ -447,6 +447,16 @@ def test_resources_have_public_backreference_to_package():
     assert package.get_resource('data').package == package
 
 
+# Save to json
+
+def test_save_as_json(json_tmpfile):
+    package = Package({})
+    package.save(json_tmpfile.name)
+    assert json.load(json_tmpfile) == {
+        'profile': 'data-package',
+    }
+
+
 # Save to zip
 
 @pytest.mark.skip('deprecated')
